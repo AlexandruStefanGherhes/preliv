2119c86d7d69abbdbf60dae673b216e8a31f3bc7
Ghesi <ghesi4ever@gmail.com>
2023-12-04 18:24:59 +0200

    case update

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,7 +2,7 @@ export const Hero = () =>{
     return(
        <div className="hero">
             <div className="hero-information">
-               <span className="hero-one">Prelucrări mecanice</span> prin așchiere de serie <span className="hero-two">medie si mică</span>
+            <span className="hero-one">Prelucrări mecanice</span> prin așchiere de serie <span className="hero-two">medie si mică</span>
             </div>
             <div className="hero-image">
                 <img src="/src/assets/cogwheels.jpg" alt="cogwheel" />
